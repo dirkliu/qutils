a466733554a43138a68f6c5d941b0d49a14cf014
刘琦 <liuqi1-iri@360.cn>
2019-05-20 14:16:28 +0800

    first commit

---
+++ date2str.js
@@ -0,0 +1,23 @@
+export default function (date) {
+    if (date.prototype.toLocaleString) {
+        return date.toLocaleString('zh-CN', { hour12: false }).replace(/\//g, '-').replace(/\b\d\b/g, '0$&')
+    } else {
+        function getDateString(date) {
+            let year = date.getFullYear()
+            let month = date.getMonth() + 1
+            let dates = date.getDate()
+            return year + '-' + (month > 9 ? month : '0' + month) + '-' + (dates > 9 ? dates : '0' + dates)
+        }
+
+        function getTimeString(date) {
+            let hours = date.getHours()
+            let minutes = date.getMinutes()
+            let seconds = date.getSeconds()
+            return (hours > 9 ? hours : '0' + hours) + ':' + (minutes > 9 ? minutes : '0' + minutes) + ':' + (seconds > 9 ? seconds : '0' + seconds)
+        }
+
+        var today = getDateString(new Date())
+        var now = getTimeString(new Date())
+        return today + ' ' + now
+    }
+}
---
+++ index.js
@@ -0,0 +1,6 @@
+import qs from './qs'
+import date2str from './date2str'
+export default {
+    qs,
+    date2str
+}
