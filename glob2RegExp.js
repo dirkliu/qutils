a412f578e0b4a289a858a7bd36bbcae9ad23268b
dirkliu <liuqi0702@126.com>
2021-04-18 22:05:43 +0800

    add glob2regexp!

---
+++ glob2RegExp.js
@@ -0,0 +1,8 @@
+function glob2RegExp (test) {
+	if (typeof test === "string") {
+		test = new RegExp("^" + test.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&"));
+	}
+	return test;
+}
+
+export default glob2RegExp
--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 import qs from './qs'
 import date2str from './date2str'
+import glob2RegExp from './glob2RegExp'
+import isDublicate from './isDublicate'
+
 export default {
     qs,
-    date2str
+    date2str,
+    glob2RegExp,
+    isDublicate
 }
